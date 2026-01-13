62dfb24bf1eb4427d41ef21756994bce4248cf15
Sinead-Manuel <08sinead.manuel01@gmail.com>
2026-01-13 12:38:54 +0000

    Added Function to Remove a Task

--- todo.py
+++ todo.py
@@ -18,8 +18,20 @@ def view_tasks():
     print("\n~ To Do List ~")
 
     # Displays all tasks in todo_list
+    i = 1
     for task in todo_list:
-        print(task["task"], "|", task["status"])
+        print(i, ":", task["task"], "|", task["status"])
+        i += 1
+
+def remove_task():
+    print("\nRemove A Task")
+    view_tasks()
+
+    task_number = int(input("\nEnter the task number to remove: "))
+
+    todo_list.pop(task_number - 1)
+
+    print("\nTask removed successfully!")
 
 
 # Main menu which reappears after an option is completed
@@ -29,16 +41,16 @@ while True:
     print("2. View tasks")
     print("3. Remove a task")
     print("4. Exit")
-    option = input("Choose a menu option: ")
+    option = int(input("Choose a menu option: "))
 
     match option:
-        case '1':
+        case 1:
             add_task()
-        case '2':
+        case 2:
             view_tasks()
-        case '3':
-            print("\nRemove A Task")
-        case '4':
+        case 3:
+            remove_task()
+        case 4:
             print("\nExiting the application. Goodbye!")
             break
         case _:
